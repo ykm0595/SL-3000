7e0cd3f29e0eda4e78a2fb2c9222acf1ce5347d5
ykm0595 <163884300+ykm0595@users.noreply.github.com>
2025-12-06 10:56:53 +0800

    Update 99-siluo-sl3000.mk

--- mk/99-siluo-sl3000.mk
+++ mk/99-siluo-sl3000.mk
@@ -2,8 +2,15 @@ define Device/siluo_sl3000
   DEVICE_VENDOR := Siluo
   DEVICE_MODEL := SL3000
   DEVICE_DTS := mt7981-siluo-sl3000
-  DEVICE_PACKAGES := kmod-mt76 kmod-mt7981-firmware
-  SUPPORTED_DEVICES := sl3000
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+
+  DEVICE_PACKAGES := \
+    kmod-mt7981-firmware \
+    kmod-mt76 kmod-mt76-connac kmod-mt76-mt7981e \
+    block-mount kmod-fs-ext4 \
+    resize2fs fdisk lsblk
+
+  IMAGES := sysupgrade.bin
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += siluo_sl3000
